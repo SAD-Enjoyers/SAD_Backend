2700d1a9bb3e276ab69cde3060363c9d4ccfd6b1
supernova-Z313 <supernova313.313z@gmail.com>
2024-11-15 16:10:01 +0330

    feat: Refresh jwt Token if needed

--- src/middleware/auth/authenticateToken.js
+++ src/middleware/auth/authenticateToken.js
@@ -1,5 +1,11 @@
 const jwt = require('jsonwebtoken');
 const { error } = require('../../utils');
+const ini = require('ini');
+const fs = require('fs');
+const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+const userExpirationTime = config.server.userExpirationTime;
+const expertExpirationTime = config.server.expertExpirationTime;
+const JWTrecoveryTime = parseInt(config.server.JWTrecoveryTime);
 const JWT_SECRET = process.env.JWT_SECRET;
 
 async function authenticateToken(req, res, next) {
@@ -10,9 +16,20 @@ async function authenticateToken(req, res, next) {
 		if (!token) return res.status(401).json(error('Access token missing.', 401));
 
 		const decoded = jwt.verify(token, JWT_SECRET);
-		req.userId = decoded.userId;
-		req.role = req.headers['Role'];
+		req.userName = decoded.userName;
+		req.role = req.headers['role'];
 
+		const currentTime = Math.floor(Date.now() / 1000);
+		const timeLeft = Math.floor((decoded.exp - currentTime)/60); // time in minute
+		req.timeLeft = timeLeft;
+		if(timeLeft <= JWTrecoveryTime && req.role == "user"){
+			const newToken = jwt.sign({ userName: decoded.userName }, JWT_SECRET, { expiresIn: userExpirationTime });
+			req.token = newToken;
+			res.setHeader('x-new-token', newToken);
+			req.updateJWT = true;
+		} else {
+			req.updateJWT = false;
+		}
 		next();
 	} catch (err) {
 		return res.status(403).json(error('Invalid or expired token.', 403));
