36284ea40e80d20d09358b513254da634027fdfe
supernova-Z313 <supernova313.313z@gmail.com>
2024-11-15 16:03:27 +0330

    feat: Retrieve user profile data

--- src/controllers/privateProfile.js
+++ src/controllers/privateProfile.js
@@ -1,6 +1,7 @@
 const { User, BackupUser, Expert } = require('../models');
 const { success, error, hashPassword, verifyPassword,
-	randomPassword, clearRecoveryCode, generateRandomToken } = require('../utils');
+	randomPassword, clearRecoveryCode, generateRandomToken, 
+	convUser, convExpert } = require('../utils');
 const { logger, transporter, createMail, forgotMail, verifyMail } = require('../configs');
 const jwt = require('jsonwebtoken');
 const ini = require('ini');
@@ -14,7 +15,22 @@ const name = config.app.name;
 const baseRoute = 'api/v1'
 
 async function getPrivateProfile (req, res) {
-	return res.send('ok');
+	if (req.role == "user"){
+		const user = await User.findOne({ where: { user_id: req.userName } });
+		if (!user) {
+			return res.status(404).json(error('User not found.', 404));
+		}
+		let userData = convUser(user);
+		return res.status(200).json(success('User Profile:', { userData } ))
+	} else if (req.role == "expert") {
+		const expert = await Expert.findOne({ where: { expert_id: req.userName } });
+		if (!user) {
+			return res.status(404).json(error('User not found.', 404));
+		}
+	} else {
+		return res.status(404).json(error('User not found.', 404));
+	}
+
 }
 
 module.exports = { getPrivateProfile };
