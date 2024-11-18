e5a871d9284014d99addd72ddc808608df1e1ce2
amirhk22 <amirhossein22kashkouie@yahoo.com>
2024-11-18 17:31:12 +0330

    feat: Add question score submission

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,4 +1,4 @@
-const { Question } = require('../models');
+const { Question, RecordedScores } = require('../models');
 const { success, error, convQuestion } = require('../utils');
 
 async function addQuestion (req, res) {
@@ -10,4 +10,30 @@ async function addQuestion (req, res) {
 	return res.status(201).json(success("Question added.", { question }));
 }
 
-module.exports = { addQuestion };
+async function scoreSubmission(req, res) {
+	let question_id = req.body.questionId;
+	let user_id = req.userName;
+	let question = await Question.findOne({ where: { question_id } });
+	if(question.user_id == req.body.userName){
+		return res.status(403).json(error("The user cannot vote on his question.", 403));
+	}
+	let scored = await RecordedScores.findOne({ where: { question_id, user_id } });
+	if(scored){
+		return res.status(403).json(error("You have already rated.", 403));
+	}
+
+	let score = req.body.scored;
+	if(question.number_of_voters){
+		question.score = ((question.score * question.number_of_voters) + score) / (question.number_of_voters + 1);
+		question.number_of_voters = question.number_of_voters + 1;
+	} else {
+		question.number_of_voters = 1;
+		question.score = score;
+	}
+	scored = await RecordedScores.create({ question_id, user_id, score });
+	question.save();
+	res.status(200).json(success("The score was recorded.", 
+		{ score: question.score, numberOfVoters: question.number_of_voters }));
+}
+
+module.exports = { addQuestion, scoreSubmission };
--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { addQuestion } = require('../controllers/question');
+const { addQuestion, scoreSubmission } = require('../controllers/question');
 const { authenticateToken } = require('../middleware');
 
 const questionRoutes = express.Router();
 
 questionRoutes.post('/add-question', authenticateToken , addQuestion);
-
+questionRoutes.put('/score-submission', authenticateToken, scoreSubmission);
 
 module.exports = questionRoutes;
