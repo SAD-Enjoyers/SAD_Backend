d0fe0a038af23df7b45b48c577b3798d078c9454
amirhk22 <amirhossein22kashkouie@yahoo.com>
2024-11-20 08:34:26 +0330

    feat: Migrate categories

---
+++ db/migrations/202411200936-add-categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO "Category" (category)
+VALUES 
+	('Linux'), ('Web Development'), ('Artificial Intelligence'),
+	('Machine Learning'), ('Data Science'), ('Networks'),
+	('Cybersecurity'), ('Cloud Computing'), ('Operating Systems'),
+	('Database Systems'), ('Software Engineering'), ('Embedded Systems'),
+	('Robotics'), ('Computer Vision'), ('Natural Language Processing'),
+	('Game Development'), ('Mobile Development'), ('DevOps'),
+	('Distributed Systems'), ('Big Data'), ('Git'), 
+	('GUI'), ('Docker');
