ba4fe31d031f67a270f0b463547acf60ebbba439
supernova-Z313 <supernova313.313z@gmail.com>
2024-11-30 19:57:54 +0330

    feat: Model for services

--- src/models/educationalService.js
+++ src/models/educationalService.js
@@ -1,5 +1,4 @@
 const { DataTypes, Sequelize } = require('sequelize');
-// const sequelize = require('../configs/db');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
@@ -10,20 +9,23 @@ const EducationalService = sequelize.define('EducationalService', {
   user_id: {
     type: DataTypes.STRING(30),
     allowNull: false,
+    unique: true,
     references: {
       model: 'User',
       key: 'user_id',
     },
-    onUpdate: 'CASCADE',
     onDelete: 'NO ACTION',
+    onUpdate: 'CASCADE',
   },
   service_id: {
-    type: DataTypes.STRING(20),
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
     allowNull: false,
+    unique: true,
     primaryKey: true,
   },
   s_name: {
-    type: DataTypes.STRING(70),
+    type: DataTypes.STRING(150),
     allowNull: false,
   },
   description: {
@@ -31,39 +33,37 @@ const EducationalService = sequelize.define('EducationalService', {
     allowNull: true,
   },
   s_level: {
-    type: DataTypes.STRING(15),
+    type: DataTypes.CHAR(1),
     allowNull: false,
-    defaultValue: 'Beginner',
     validate: {
-      isIn: [['Beginner', 'Medium', 'Advanced']],
+      isIn: [['1', '2', '3']], // 1: Beginner, 2: Medium, 3: Advanced
     },
   },
   price: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false,
+    type: DataTypes.DECIMAL(6, 2), // Matches NUMERIC(6, 2)
+    defaultValue: 0.0,
   },
   service_type: {
-    type: DataTypes.SMALLINT,
+    type: DataTypes.CHAR(1),
     allowNull: false,
     validate: {
-      isIn: [[1, 2, 3]],
+      isIn: [['1', '2', '3']], // 1: Exam, 2: Article, 3: Video
     },
   },
   activity_status: {
     type: DataTypes.CHAR(1),
-    allowNull: false,
-    defaultValue: 'P',
+    defaultValue: 'P', // A: Active, S: Suspended, P: Passive
     validate: {
       isIn: [['A', 'S', 'P']],
     },
   },
-  achieved_score: {
-    type: DataTypes.FLOAT,
+  score: {
+    type: DataTypes.DECIMAL(3, 2),
     allowNull: false,
-    defaultValue: 0,
+    defaultValue: 0.0,
   },
-  achieved_from: {
-    type: DataTypes.INTEGER,
+  number_of_voters: {
+    type: DataTypes.SMALLINT,
     allowNull: false,
     defaultValue: 0,
   },
@@ -71,9 +71,53 @@ const EducationalService = sequelize.define('EducationalService', {
     type: DataTypes.STRING(250),
     allowNull: true,
   },
+  tag1: {
+    type: DataTypes.STRING(30),
+    allowNull: true,
+  },
+  tag2: {
+    type: DataTypes.STRING(30),
+    allowNull: true,
+  },
+  tag3: {
+    type: DataTypes.STRING(30),
+    allowNull: true,
+  },
 }, {
-  timestamps: false,
   tableName: 'Educational_service',
+  timestamps: false,
 });
 
-module.exports = { EducationalService };
+const ServiceRecordedScores = sequelize.define('ServiceRecordedScores', {
+  service_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Educational_service',
+      key: 'service_id',
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    references: {
+      model: 'User',
+      key: 'user_id',
+    },
+    onDelete: 'NO ACTION',
+    onUpdate: 'CASCADE',
+    primaryKey: true,
+  },
+  score: {
+    type: DataTypes.DECIMAL(3, 2),
+    allowNull: false,
+  },
+}, {
+  tableName: 'Service_recorded_scores',
+  timestamps: false,
+});
+
+module.exports = { EducationalService, ServiceRecordedScores };
