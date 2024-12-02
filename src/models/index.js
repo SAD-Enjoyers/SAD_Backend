745d38f5f57dbc5fd946bb03ca8f1699e0bf19e7
amirhk22 <amirhossein22kashkouie@yahoo.com>
2024-12-02 18:12:41 +0330

    fix: Add missed relation of question and selectedQuestions

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,18 @@ const category = require('./category');
 const question = require('./question');
 const exam = require('./exam');
 
+exam.SelectedQuestions.belongsTo(question.Question, {
+	foreignKey: 'question_id',
+	onDelete: 'CASCADE',
+	onUpdate: 'CASCADE',
+});
+
+question.Question.hasMany(exam.SelectedQuestions, {
+	foreignKey: 'question_id',
+	onDelete: 'CASCADE',
+	onUpdate: 'CASCADE',
+});
+
 module.exports = {
 	...user,
 	...expert,
