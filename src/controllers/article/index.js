2651eab0aec86a6ee3c6b646d3b819d127062684
supernova-Z313 <supernova313.313z@gmail.com>
2024-12-19 19:03:05 +0330

    fix: Fix export article page

--- src/controllers/article/index.js
+++ src/controllers/article/index.js
@@ -1,9 +1,7 @@
 const article = require('./article');
 const privateArticle = require('./privateArticlePage');
-const publicArticle = require('./publicArticlePage');
 
 module.exports = {
 	...article,
 	...privateArticle,
-	...publicArticle,
 }
