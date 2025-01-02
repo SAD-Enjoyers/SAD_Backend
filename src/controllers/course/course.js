49a641260758b67c14e619e93cc1a032aa07ef95
supernova-Z313 <supernova313.313z@gmail.com>
2025-01-02 21:08:49 +0330

    feat: Search and filter courses

--- src/controllers/course/course.js
+++ src/controllers/course/course.js
@@ -1,9 +1,83 @@
 const { EducationalService, Registers, Video, User } = require('../../models');
-const { success, error, convCourse } = require('../../utils');
+const { success, error, convCourse, convPreviewExam } = require('../../utils');
+const { Op } = require('sequelize');
 const { sequelize } = require('../../configs');
 
 async function courses(req, res) {
-	// body...
+	const { search, tags, sort, level } = req.query;
+
+	let s_level = '1';
+	if(level){
+		if (level == 'Beginner') s_level = '1';
+		else if (level == 'Medium') s_level = '2';
+		else if (level == 'Advanced') s_level = '3';
+		else return res.status(400).json(error('Level is not valid.', 400 ));
+	} else 
+		s_level = [ '1', '2', '3' ];
+
+	let filters = {};
+	if(req.partialAccess){
+		filters = {
+			[Op.or]: [
+				{ activity_status: 'A' },
+			], [Op.and]: [
+				{ service_type: '3', s_level: s_level },
+			],
+		};
+	} else {
+		filters = {
+			[Op.or]: [
+				{ activity_status: 'A' },
+				{ user_id: req.userName },
+			], [Op.and]: [
+				{ service_type: '3', s_level: s_level },
+			],
+		};
+	}
+
+	if (search) {
+		filters.s_name = {
+			[Op.iLike]: `%${search}%`,
+		};
+	}
+
+	if (tags) {
+		const tagList = tags.split(',');
+		filters[Op.and] = [
+			...filters[Op.and] || [],
+			{
+				[Op.or]: tagList.map(tag => ({
+					[Op.or]: [
+						{ tag1: tag },
+						{ tag2: tag },
+						{ tag3: tag },
+					],
+				})),
+			},
+		];
+	}
+
+	const order = [];
+	if (sort) {
+		let sTemp = sort.split('-');
+		if (sTemp[0] == "score"){
+			order.push(sTemp);
+		} else {
+			order.push(["s_name", sTemp[1]]);
+		}
+	}
+
+	const course = await EducationalService.findAll({
+		where: filters,
+		order,
+	});
+
+	let result = [];
+	for (let e of course){
+		result.push(convPreviewExam(e.dataValues));
+	}
+
+	return res.status(200).json(success("course", { result }));
 }
 
 async function preview(req, res) {
---
+++ src/controllers/course/publicCourse.js
@@ -0,0 +1,4 @@
+const { EducationalService, Registers, Video, User } = require('../../models');
+const { success, error, convCourse } = require('../../utils');
+const { sequelize } = require('../../configs');
+
