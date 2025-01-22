14a2591ccca9771555f465fa23ffb4a54aadb2c4
supernova-Z313 <supernova313.313z@gmail.com>
2025-01-23 03:03:46 +0330

    feat: Register ticket from user

--- src/controllers/commonController.js
+++ src/controllers/commonController.js
@@ -1,5 +1,5 @@
-const { Category } = require('../models');
-const { success, error, convCategory } = require('../utils');
+const { Category, Ticket, Registers, EducationalService } = require('../models');
+const { success, error, convCategory, convTicket } = require('../utils');
 
 async function categories(req, res) {
 	let categoryList = await Category.findAll();
@@ -15,4 +15,27 @@ async function uploadFile(req, res) {
 	res.status(200).json(success('Uploaded successfully.', { fileName: req.file.filename} ));
 }
 
-module.exports = { categories, uploadFile };
+async function registerTicket(req, res) {
+	if (req.role == 'user'){
+		if (!req.body.serviceId || !req.body.message)
+			return res.status(404).json(error('ServiceId and message required.', 404));
+		
+		const edu = await EducationalService.findOne({ where: { service_id: req.body.serviceId } });
+		if (!edu)
+			return res.status(404).json(error('Service not found.', 404));
+		const reg = await Registers.findOne({ where: { user_id: req.userName, service_id: req.body.serviceId } });
+		if(!reg && edu.user_id != req.userName) 
+			return res.status(404).json(error('You are not registered in this service.', 404));
+
+		let ticket = await Ticket.findOne({ where: { user_id: req.userName, service_id: req.body.serviceId, t_state: 1 } });
+		if (ticket)
+			return res.status(403).json(error('Your latest ticket for this service is under review.', 403));
+		
+		ticket = await Ticket.create({ user_id: req.userName, service_id: req.body.serviceId,
+			t_message: req.body.message, t_state: 1, report_time: new Date() });
+		res.status(201).json(success('Ticket successfully registered.', convTicket(ticket)));
+	} else 
+		return res.status(403).json(error('Permission denied.', 403));
+}
+
+module.exports = { categories, uploadFile, registerTicket };
--- src/routes/commonRoutes.js
+++ src/routes/commonRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { categories } = require('../controllers/commonController');
+const { categories, registerTicket } = require('../controllers/commonController');
 const { authenticateToken } = require('../middleware');
 
 const commonRoutes = express.Router();
 
 commonRoutes.get('/categories', categories);
+commonRoutes.post('/register-ticket', authenticateToken, registerTicket);
 
 module.exports = commonRoutes;
