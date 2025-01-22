ede1d474fee081f0d0276d86fb8c35c7e2c03c81
supernova-Z313 <supernova313.313z@gmail.com>
2025-01-23 01:44:00 +0330

    feat: Search on tickets

--- src/controllers/admin/ticket.js
+++ src/controllers/admin/ticket.js
@@ -4,6 +4,26 @@ const { logger, transporter, createMail, forgotMail, verifyMail } = require('../
 const { Op } = require('sequelize');
 
 async function searchTicket(req, res) {
+	if(req.role == 'expert'){
+		const { ticketId, userId } = req.query;
+		if (!ticketId && !userId) 
+			return res.status(400).json(error('Please provide either ticketId or userId to search.', 400));
+
+		const filters = {};
+		if (ticketId)
+			filters.ticket_id = ticketId;
+		if (userId)
+			filters.user_id = userId;
+
+		let tickets = await Ticket.findAll({ where: filters, });
+		if (tickets.length === 0) {
+			return res.status(404).json(error('No tickets found matching the criteria.', 404));
+		}
+		tickets = tickets.map((item) => ({ ...convTicket(item) }));
+
+		res.status(200).json(success('Tickets retrieved successfully.', tickets));
+	} else 
+		return res.status(403).json(error('Access denied.', 403));
 }
 
 async function newTicket(req, res) {
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -8,5 +8,6 @@ const adminRoutes = express.Router();
 adminRoutes.post('/edit-admin', authenticateToken , editExpert);
 adminRoutes.get('/profile', authenticateToken, getPrivateProfile);
 adminRoutes.get('/tickets', authenticateToken, newTicket)
+adminRoutes.get('/search-tickets', authenticateToken, searchTicket);
 
 module.exports = adminRoutes;
